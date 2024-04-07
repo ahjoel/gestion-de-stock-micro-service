e23e2abce31ac0eef34c41fa1debfadf6f1e115f
jaholou <joel.aholou@togocom.tg>
2024-04-07 15:53:15 +0000

    initial commit

---
+++ commande/commande/src/main/java/com/gestock/fourniture/http_client/FournitureRestClient.java
@@ -0,0 +1,80 @@
+package com.gestock.fourniture.http_client;
+
+import com.gestock.fourniture.model.dto.CategorieDto;
+import com.gestock.fourniture.model.dto.CommandeDto;
+import com.gestock.fourniture.model.dto.EmployeDto;
+import com.gestock.fourniture.model.dto.FournitureDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(name = "FOURNITURE-SERVICE")
+public interface FournitureRestClient {
+
+    // CATEGORIES API(s) CALL
+    @GetMapping("/api/categories")
+    List<CategorieDto> listeCategorie();
+
+    @PostMapping("/api/categorie/addcat")
+    Long enregistrerCategorie(@RequestBody CategorieDto categorieDto);
+
+    @GetMapping("/api/categorie/{id}")
+    CategorieDto afficherCategorie(@PathVariable("id") Long id);
+
+    @PutMapping("/api/categorie/modifiercat")
+    boolean modifierCategorie(@RequestBody CategorieDto categorieDto);
+
+    @DeleteMapping("/api/categorie/delete/{id}")
+    boolean deleteCategorie(@PathVariable("id") Long id);
+
+
+    // COMMANDES API(s) CALL
+    @GetMapping("/api/commandes")
+    List<CommandeDto> listeCommande();
+
+    @PostMapping("/api/commande/addcom")
+    CommandeDto enregistrerCommande(@RequestBody CommandeDto commandeDto);
+
+    @GetMapping("/api/commande/{id}")
+    CommandeDto afficherCommande(@PathVariable("id") Long id);
+
+    @DeleteMapping("/api/commande/delete/{id}")
+    Boolean supprimerCommande(@PathVariable("id") Long id);
+
+
+    // EMPLOYES API(s) CALL
+    @GetMapping("/api/employes")
+    List<EmployeDto> listeEmploye();
+
+    @PostMapping("/api/employe/addemp")
+    Long enregistrerEmploye(@RequestBody EmployeDto employeDto);
+
+    @GetMapping("/api/employe/{id}")
+    EmployeDto afficherEmploye(@PathVariable("id") Long id);
+
+    @PutMapping("/api/employe/modifieremp")
+    boolean modifierEmploye(@RequestBody EmployeDto employeDto);
+
+    @DeleteMapping("/api/employe/delete/{id}")
+    boolean deleteEmploye(@PathVariable("id") Long id);
+
+
+    // FOURNITURES API(s) CALL
+    @GetMapping("/api/fournitures")
+    List<FournitureDto> listeFourniture();
+
+    @PostMapping("/api/fourniture/addfour")
+    Long enregistrerFourniture(@RequestBody FournitureDto fournitureDto);
+
+    @GetMapping("/api/fourniture/{id}}")
+    FournitureDto afficherFourniture(@PathVariable("id") Long id);
+
+    @PutMapping("/api/fourniture/modifierfour")
+    Boolean modifierFourniture(@RequestBody FournitureDto fournitureDto);
+
+    @DeleteMapping("/api/fourniture/delete/{id}")
+    Boolean supprimerFourniture(@PathVariable("id") Long id);
+
+}
---
+++ readme.md
@@ -0,0 +1,29 @@
+# GESTION DE STOCK DES FOURNITURES DE BUREAUX - ARCHITECTURE MICROSERVICE
+
+# Procédure de generation des livrables
+
+- Installez Java 17 sur votre machine
+
+- Ouvrez le projet account-service avec un IDE : executez mvn clean package -DskipTests=true
+
+- Récuperez le livrable .jar dans le dossier target du projet account-service
+
+- Ouvrez le projet fournitures avec un IDE : executez mvn clean package -DskipTests=true
+
+- Récuperez le livrable .jar dans le dossier target du projet fournitures
+
+- Ouvrez le projet commandes avec un IDE : executez mvn clean package -DskipTests=true
+
+- Récuperez le livrable .jar dans le dossier target du projet commandes
+
+- Ouvrez le projet config-service avec un IDE : executez mvn clean package -DskipTests=true
+
+- Récuperez le livrable .jar dans le dossier target du projet config-service
+
+- Ouvrez le projet discovery-service avec un IDE : executez mvn clean package -DskipTests=true
+
+- Récuperez le livrable .jar dans le dossier target du projet discovery-service
+
+- Ouvrez le projet gateway-service avec un IDE : executez mvn clean package -DskipTests=true
+
+- Récuperez le livrable .jar dans le dossier target du projet gateway-service
